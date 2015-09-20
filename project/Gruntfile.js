4f19bc6790d8845ab60d248b69f0273fdd046b5b
Sebastian Coetzee <mail@sebastiancoetzee.com>
2015-09-20 01:13:43 +0000

    Second commit.

--- project/Gruntfile.js
+++ project/Gruntfile.js
@@ -3,14 +3,41 @@ module.exports = function(grunt) {
 	grunt.initConfig(
 		{
 			exec: {
+				jadeProd: "node node_modules/.bin/jade src/public --out build --hierarchy",
 				jadeDev: "node node_modules/.bin/jade src/public --out build --hierarchy --pretty"
+			},
+			less: {
+				dev: {
+					files: {
+						"build/css/style.css": "src/less/style.less"
+					}
+				}
+			},
+			copy: {
+				dev: {
+					src: "src/js/app.js",
+					dest: "build/js/app.js"
+				}
+			},
+			uglify: {
+				prod: {
+					options: {
+						sourceMap: false
+					},
+					files: {
+						'build/js/app.js': ['src/js/app.js']
+					}
+				}
 			}
 		}
 	);
 
 	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['jshint']);
-	grunt.registerTask('dev', ['exec:jadeDev']);
+	grunt.registerTask('default', ['less', 'exec:jadeProd', 'uglify:prod']);
+	grunt.registerTask('dev', ['less', 'exec:jadeDev', 'copy:dev']);
 
 };
---
+++ project/src/js/app.js
@@ -0,0 +1,111 @@
+//bizwrap v1.3
+
+
+
+$( window ).resize(function() {
+    $(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
+});
+/* ==============================================
+ Sticky Navbar
+ =============================================== */
+
+$(document).ready(function() {
+    $(".sticky").sticky({topSpacing: 0});
+});
+
+
+/*=========================*/
+/*====main navigation hover dropdown====*/
+/*==========================*/
+$(document).ready(function() {
+
+    $('.js-activated').dropdownHover({
+        instantlyCloseOthers: false,
+        delay: 0
+    }).dropdown();
+
+});
+
+/*====flex  slider for main slider or testimonials====*/
+$('.main-flex-slider,.testimonials').flexslider({
+    slideshowSpeed: 5000,
+    directionNav: false,
+    animation: "fade"
+});
+
+/*========portfolio mix====*/
+$('#grid').mixitup();
+
+/*========tooltip and popovers====*/
+
+$("[data-toggle=popover]").popover();
+
+$("[data-toggle=tooltip]").tooltip();
+
+
+/*========flex-gallery slide====*/
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        directionNav: false,
+        slideshowSpeed: 3000,
+        animation: "fade"
+    });
+});
+
+/*=========================*/
+     /*========Animation on scroll with wow.js====*/
+     /*==========================*/
+     
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        mobile:       true
+      }
+    );
+    wow.init();
+    
+    /* -------------------
+ Parallax Sections
+ ---------------------*/
+$('.parallax-1').parallax("50%", 0.5);
+$('.page-tree-bg').parallax("50%", 0.5);
+
+(function(){
+	
+	var myLatlng;
+	var map;
+	var marker;
+	
+	function initialize() {
+		myLatlng = new google.maps.LatLng(-33.937952, 18.862212);
+	
+		var mapOptions = {
+			zoom: 17,
+			center: myLatlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			scrollwheel: false,
+			draggable: false
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
+		var contentString = '<p style="line-height: 20px;"><strong>Coastal Concepts</strong></p><p>Old College Building, 35 Church Street, Stellenbosch</p>';
+	
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString
+		});
+	
+		marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'Marker'
+		});
+	
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map, marker);
+		});
+	}
+	
+	google.maps.event.addDomListener(window, 'load', initialize);
+	
+}).call(this);
